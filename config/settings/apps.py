0a9969d0559cd856c847a4f7710d4a5829516b8a
JDgresse <jdgresse@rather.chat>
2025-07-28 12:07:34 +0200

    Added: ->
    Updated:
    -> config/settings/apps.py
    -> config/settings/base.py

--- config/settings/apps.py
+++ config/settings/apps.py
@@ -1,1 +1,29 @@
-# apps settings
+"""
+Base Django apps, third-party apps, and local apps are categorized for better organization.
+
+Define and organize Django apps into groups:
+- DJANGO_APPS
+- THIRD_PARTY_APPS
+- LOCAL_APPS
+"""
+
+# Base Django apps
+DJANGO_APPS = [
+        'django.contrib.admin',
+        'django.contrib.auth',
+        'django.contrib.contenttypes',
+        'django.contrib.sessions',
+        'django.contrib.messages',
+        'django.contrib.staticfiles',
+        ]
+
+# Third-party apps
+THIRD_PARTY_APPS = [
+        'rest_framework',
+        ]
+
+# Apps you create (Local apps)
+LOCAL_APPS = [
+        'apps.custom_user',
+        'apps.dashboard',
+        ]
